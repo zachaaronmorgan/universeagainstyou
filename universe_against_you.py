dde5d296db37f4c60c7e0a00e61883323de92982
zachaaronmorgan <143377844+zachaaronmorgan@users.noreply.github.com>
2024-11-14 15:09:29 -0500

    Installed pygame properly to ensure the dependencies are up to date in pipfile.lock and updated game files

--- universe_against_you.py
+++ universe_against_you.py
@@ -1,7 +1,7 @@
 import sys
 import pygame
-from settings import Settings
 from ship import Ship
+from settings import Settings
 
 class UniverseAgainstYou:
     """Overall class to manage game assets and behavior."""
@@ -9,12 +9,13 @@ class UniverseAgainstYou:
     def __init__(self):
         """Initialize the game, and create game resources."""
         pygame.init()
+        self.ship = Ship(self)
         self.clock = pygame.time.Clock()
         self.settings = Settings()
-        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
         
         pygame.display.set_caption("Universe Against You")
-        self.ship = Ship(self)
         self.bg_color = (230, 230, 230)
 
     def run_game(self):

8de34ef35b5a7f185f0201de0b5dccef946e54b6
zachaaronmorgan@gmail.com <zachaaronmorgan@gmail.com>
2024-11-15 12:19:56 -0500

    refactored to simplify checking for keyboard input and updating the screen

--- universe_against_you.py
+++ universe_against_you.py
@@ -21,18 +21,27 @@ class UniverseAgainstYou:
     def run_game(self):
         """Start the main loop for the game."""
         while True:
-            # Watch for keyboard and mouse events.
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-            
-            self.screen.fill(self.settings.bg_color)
-            
-            self.ship.blitme()
-
-            # Make the most recently drawn screen visible.
-            pygame.display.flip()
+            self._check_events()
+            self._update_screen()
             self.clock.tick(60)
+          
+    
+    def _check_events(self):
+        """Respond to keypresses and mouse events."""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+    
+    def _update_screen(self):
+        """Update images on the screen, and flip to the new screen."""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        pygame.display.flip()
+        
 if __name__ == '__main__':
     # Make a game instance, and run the game.
     uay = UniverseAgainstYou()
